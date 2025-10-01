2996b62799dd82f9493f71cb75f8886428667f97
YPrtm <137468791+yprtm-195@users.noreply.github.com>
2025-10-01 17:31:44 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+docment.addEventListener('DOMContentLoaded', () => {
 
     // --- Global Helper Functions ---
     const getFormattedDate = () => {
